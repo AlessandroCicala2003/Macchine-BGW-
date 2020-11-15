6a25cce6fa9b403e4945b814925657be6a88e4d0
AlessandroCicala2003 <64033968+AlessandroCicala2003@users.noreply.github.com>
2020-11-15 15:54:35 +0100

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Macchine_BGW_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.imgNera = new System.Windows.Forms.PictureBox();
+            this.imgRossa = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.imgTraguardo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgNera)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgRossa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgTraguardo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(392, 386);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(105, 52);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // imgNera
+            // 
+            this.imgNera.Image = global::Macchine_BGW_v2.Properties.Resources.macchinabianca__1_;
+            this.imgNera.Location = new System.Drawing.Point(15, 63);
+            this.imgNera.Name = "imgNera";
+            this.imgNera.Size = new System.Drawing.Size(128, 73);
+            this.imgNera.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgNera.TabIndex = 2;
+            this.imgNera.TabStop = false;
+            // 
+            // imgRossa
+            // 
+            this.imgRossa.Image = global::Macchine_BGW_v2.Properties.Resources.macchinarossa__1_;
+            this.imgRossa.Location = new System.Drawing.Point(15, 240);
+            this.imgRossa.Name = "imgRossa";
+            this.imgRossa.Size = new System.Drawing.Size(128, 73);
+            this.imgRossa.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgRossa.TabIndex = 3;
+            this.imgRossa.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Macchine_BGW_v2.Properties.Resources.road_trip_background_10864211__1_;
+            this.pictureBox3.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(873, 381);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // imgTraguardo
+            // 
+            this.imgTraguardo.Image = global::Macchine_BGW_v2.Properties.Resources.finishline;
+            this.imgTraguardo.Location = new System.Drawing.Point(785, -1);
+            this.imgTraguardo.Name = "imgTraguardo";
+            this.imgTraguardo.Size = new System.Drawing.Size(74, 381);
+            this.imgTraguardo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgTraguardo.TabIndex = 5;
+            this.imgTraguardo.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(871, 450);
+            this.Controls.Add(this.imgNera);
+            this.Controls.Add(this.imgRossa);
+            this.Controls.Add(this.imgTraguardo);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBox3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imgNera)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgRossa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgTraguardo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox imgNera;
+        private System.Windows.Forms.PictureBox imgRossa;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox imgTraguardo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Macchine_BGW_v2
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker bgw1, bgw2;
+        int xn0, yn0, xr0, yr0;
+        Random rnd;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            xn0 = imgNera.Location.X;
+            yn0 = imgNera.Location.Y;
+            xr0 = imgRossa.Location.X;
+            yr0 = imgRossa.Location.Y;
+            rnd = new Random();
+        }
+
+       
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            imgNera.Location = new Point(xn0, yn0);
+            imgRossa.Location = new Point(xn0, yr0);
+            bgw1 = new BackgroundWorker();
+            bgw1.WorkerReportsProgress = true;
+            bgw1.WorkerSupportsCancellation = true;
+            bgw1.DoWork += Bgw_DoWork;
+            bgw1.ProgressChanged += Bgw_ProgressChanged;
+            bgw1.RunWorkerCompleted += Bgw_RunWorkerCompleted;
+
+            bgw2 = new BackgroundWorker();
+            bgw2.WorkerReportsProgress = true;
+            bgw2.WorkerSupportsCancellation = true;
+            bgw2.DoWork += Bgw_DoWork;
+            bgw2.ProgressChanged += Bgw_ProgressChanged;
+            bgw2.RunWorkerCompleted += Bgw_RunWorkerCompleted;
+
+            bgw1.RunWorkerAsync(imgNera);
+            bgw2.RunWorkerAsync(imgRossa);
+        }
+
+        private void Bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            
+        }
+
+        private void Bgw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            btnStart.Enabled = false;
+            if (e.ProgressPercentage == -1)
+            {
+                PictureBox p1 = e.UserState as PictureBox;
+                if (p1.Name == "imgNera")
+                    MessageBox.Show("WINNER: Nero");
+                else if (p1.Name == "imgRossa")
+                    MessageBox.Show("WINNER: Rosso");
+                btnStart.Enabled = true;
+                bgw1.CancelAsync();
+                bgw2.CancelAsync();
+
+            }
+            else
+            {
+                PictureBox p = e.UserState as PictureBox;
+                int n = e.ProgressPercentage;
+                p.Location = new Point(p.Location.X + n, p.Location.Y);
+            }
+
+            
+        }
+
+        private void Bgw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+            BackgroundWorker bgw = sender as BackgroundWorker;
+            PictureBox img = (PictureBox)e.Argument;
+            while(img.Location.X < imgTraguardo.Location.X-135)
+            {
+                
+                if (bgw.CancellationPending)
+                {
+                    return;
+                }
+                else
+                {
+                    Thread.Sleep(70);
+                    int n1 = rnd.Next(0, 30);
+                    bgw.ReportProgress(n1, img);
+                }
+            }
+
+            if (!bgw.CancellationPending)
+                bgw.ReportProgress(-1, img);
+
+            bgw1.CancelAsync();
+            bgw2.CancelAsync();
+
+
+        }
+
+        
+    }
+}
